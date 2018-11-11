8712132fcbb9d12e0e49f51e0d58363fc5204ec2
ObsidianSwan <hollyboothroyd@hotmail.com>
2018-11-11 12:33:12 +0000

    General game infrastructure and polish
    - Added player score and health
    - Added start and game over pages
    - Added additional visual scrolling
    - Added particle effects for enemy explosions

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSession : MonoBehaviour {
+
+    int score = 0;
+    int health;
+
+	// Use this for initialization
+	void Start () {
+        SetUpSingleton();
+	}
+
+    private void SetUpSingleton()
+    {
+        int numGameSessions = FindObjectsOfType<GameSession>().Length;
+        if(numGameSessions > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+            health = FindObjectOfType<Player>().GetHealth();
+        }
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    public void AddToScore(int scoreValue)
+    {
+        score += scoreValue;
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+
+    public int GetHealth()
+    {
+        return health;
+    }
+
+    public void SetHealth(int healthValue)
+    {
+        health = healthValue;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
